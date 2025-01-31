570f352573466d912d818ea7a678aeca9388754c
Josie Cialdini <jcialdini@my.wctc.edu>
2025-01-30 20:04:44 -0600

    [1.04] Add discount title and code using attributes

--- discount.js
+++ discount.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
         if (e.target.classList.contains('discount')) {
             e.preventDefault();
             document.getElementById('product').innerHTML = e.target.dataset['product'];
+            document.getElementById('discount-title').innerText = e.target.dataset['title'];
+            document.getElementById('discount-code').innerText = e.target.dataset['discount'];
             bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
         }
     });
