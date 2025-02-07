86bfb2d9063650ae8d573f46343b851755246655
Josie Cialdini <jcialdini@my.wctc.edu>
2025-02-06 20:49:55 -0600

    [2.01] add check and uncheck buttons

--- balloons.js
+++ balloons.js
@@ -5,4 +5,28 @@ document.addEventListener("DOMContentLoaded", function () {
         autohide: true,
         format: 'MM-dd'
     });
+
+    // uncheck all boxes by default (Firefox)
+    document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
+
+    // event listener for check/uncheck
+    document.getElementById('checkbox-card').addEventListener('change', function (e) {
+        if (e.target.classList.contains('form-check-input')) {
+            const elem = document.getElementById(e.target.id + 'Img');
+            elem.style.visibility = "visible";
+            elem.classList.remove("animate__animated", "animate__bounceInDown", "animate__bounceOutUp");
+            e.target.checked ?
+                elem.classList.add("animate__animated", "animate__bounceInDown") :
+                elem.classList.add("animate__animated", "animate__bounceOutUp");
+        }
+    });
+
+    //event listeners for check and uncheck all
+    document.getElementById('check-all').addEventListener('click', function () {
+        document.querySelectorAll('.form-check-input').forEach(c => c.checked = true);
+    });
+    document.getElementById('uncheck-all').addEventListener('click', function () {
+        document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
+    });
+
 });
