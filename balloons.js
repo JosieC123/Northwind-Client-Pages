3d0e4b3da6754f99dd2a758d97b99d2c16859abe
Josie Cialdini <jcialdini@my.wctc.edu>
2025-02-13 15:09:34 -0600

    [2.03] -added toast and random animation to title. --fixed hover and balloon animation. added all colors.

--- balloons.js
+++ balloons.js
@@ -21,25 +21,35 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    //event listeners for check and uncheck all
-    document.getElementById('check-all').addEventListener('click', function () {
-        document.querySelectorAll('.form-check-input').forEach(c => c.checked = true);
+// event listeners for check and uncheck all
+document.getElementById('check-all').addEventListener('click', function () {
+    document.querySelectorAll('.form-check-input').forEach(c => {
+        c.checked = true;
+
+        //show balloons
+        const balloon = document.getElementById(c.id + 'Img');
+        balloon.style.visibility = "visible";
+        balloon.classList.remove("animate__animated", "animate__bounceOutUp");
+        balloon.classList.add("animate__animated", "animate__bounceInDown");
     });
-    document.getElementById('uncheck-all').addEventListener('click', function () {
-        document.querySelectorAll('.form-check-input').forEach(c => c.checked = false);
+});
+document.getElementById('uncheck-all').addEventListener('click', function () {
+    document.querySelectorAll('.form-check-input').forEach(c => {
+        c.checked = false;
+
+        //hide ballloons
+        const balloon = document.getElementById(c.id + 'Img');
+        balloon.classList.remove("animate__animated", "animate__bounceInDown");
+        balloon.classList.add("animate__animated", "animate__bounceOutUp");
     });
+});
 
     //hover over buttons
     document.getElementById('checkbox-card').addEventListener('mouseover', function (e) {
         if (e.target.classList.contains('form-check-label')) {
             const hover = document.querySelector('.greeting');
-            if (e.target.id == 'brownLabel') {
-                hover.style.color = 'brown';
-            } else if (e.target.id == 'greenLabel') {
-                hover.style.color = 'green';
-            } else if (e.target.id == 'cyanLabel') {
-                hover.style.color = 'cyan';
-            }
+            const color = e.target.dataset['color'];
+            hover.style.color = color;
         }
     });
     document.getElementById('checkbox-card').addEventListener('mouseout', function (e) {
@@ -48,5 +58,30 @@ document.addEventListener("DOMContentLoaded", function () {
             hover.style.color = 'slategray';
         }
     });
+
+    //Toast popup
+    document.getElementById('submit').addEventListener('click', function(e){
+        const isChecked = false;
+        const checkboxs = document.querySelectorAll('.form-check-input');
+
+        checkboxs.forEach((checkbox) => {
+            if(checkbox.checked){
+                isChecked = true;
+            }
+        })
+        if (!isChecked){
+            bootstrap.Toast.getOrCreateInstance(document.getElementById('liveToast')).show();
+        }
+    })
+
+    //random title animation
+    const animationList = ['animate__bounce','animate__flash','animate__pulse','animate__rubberBand',
+        'animate__shakeX','animate__shakeY','animate__headShake',
+        'animate__swing','animate__tada','animate__wobble','animate__jello','animate__heartBeat'];
+
+        var pickedAnimation = animationList[Math.floor(Math.random()*animationList.length)];
+
+        const title = document.querySelector('.greeting');
+        title.classList.add(pickedAnimation);
 
 });
